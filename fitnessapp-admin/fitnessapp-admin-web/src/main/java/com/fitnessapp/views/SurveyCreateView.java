6571be5aad9ae018fd5046ff9b9e91977a21da09
Erox <trugove@hotmail.com>
2018-11-28 20:32:01 +0100

    Fixed the init into SurveyCreateView

--- fitnessapp-admin/fitnessapp-admin-web/src/main/java/com/fitnessapp/views/SurveyCreateView.java
+++ fitnessapp-admin/fitnessapp-admin-web/src/main/java/com/fitnessapp/views/SurveyCreateView.java
@@ -20,6 +20,7 @@ import com.fitnessapp.api.client.SurveyClient;
 import com.fitnessapp.api.entities.Survey;
 import com.fitnessapp.api.entities.Tag;
 import java.io.Serializable;
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
@@ -38,6 +39,7 @@ public class SurveyCreateView implements Serializable {
     private String description;
     private Integer tagId;
 
+    @PostConstruct
     public void init(){
         client = new SurveyClient();
     }
