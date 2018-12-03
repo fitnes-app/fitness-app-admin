8463ccb0ccf7edbfa578dc346385a1f930e474fb
Jordi Godia <jgm17@alumnes.udl.cat>
2018-12-03 23:42:13 +0100

    API/Admin integration done for Question entity

--- fitnessapp-admin/fitnessapp-admin-web/src/main/java/com/fitnessapp/views/QuestionCreateView.java
+++ fitnessapp-admin/fitnessapp-admin-web/src/main/java/com/fitnessapp/views/QuestionCreateView.java
@@ -21,11 +21,14 @@ import com.fitnessapp.api.client.SurveyClient;
 import com.fitnessapp.api.entities.Question;
 import com.fitnessapp.api.entities.Survey;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.ws.rs.core.GenericType;
 
 /**
  *
@@ -35,15 +38,27 @@ import javax.inject.Named;
 @ViewScoped
 public class QuestionCreateView implements Serializable{
 
-    private String text;
-    private Integer surveyId;
     
-    //private Question question = new Question();
+    private Question question = new Question();
+    private String text;
     private QuestionClient questionClient = new QuestionClient();
+    
     private SurveyClient surveyClient = new SurveyClient();
+    private Survey survey = new Survey();
+    private List<Survey> surveys = new ArrayList<Survey>();
     
     @PostConstruct
     public void init() {
+        surveys = surveyClient.findAll(new GenericType<List<Survey>>() {
+        });
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
     }
 
     public String getText() {
@@ -54,29 +69,29 @@ public class QuestionCreateView implements Serializable{
         this.text = text;
     }
 
-    public Integer getSurveyId() {
-        return surveyId;
+    public Survey getSurvey() {
+        return survey;
     }
 
-    public void setSurveyId(Integer surveyId) {
-        this.surveyId = surveyId;
+    public void setSurvey(Survey survey) {
+        this.survey = survey;
     }
 
-//    public Question getQuestion() {
-//        return question;
-//    }
-//
-//    public void setQuestion(Question question) {
-//        this.question = question;
-//    }
-    
+    public List<Survey> getSurveys() {
+        return surveys;
+    }
+
+    public void setSurveys(List<Survey> surveys) {
+        this.surveys = surveys;
+    }
+
+
     public void save() {
-        Question question = new Question();
-        if(text!=null && surveyId!=null){
+        
             question.setText(text);
-            question.setSurvey(surveyClient.find(Survey.class, surveyId.toString()));
+            question.setSurvey(survey);
             questionClient.create(question);
-        }
+        
         
         addMessage("New Question added");
     }
--- fitnessapp-admin/fitnessapp-admin-web/src/main/java/com/fitnessapp/views/QuestionEditView.java
+++ fitnessapp-admin/fitnessapp-admin-web/src/main/java/com/fitnessapp/views/QuestionEditView.java
@@ -17,13 +17,17 @@
 package com.fitnessapp.views;
 
 import com.fitnessapp.api.client.QuestionClient;
+import com.fitnessapp.api.client.SurveyClient;
 import com.fitnessapp.api.entities.Question;
+import com.fitnessapp.api.entities.Survey;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
+import javax.faces.component.UIOutput;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.ws.rs.core.GenericType;
@@ -41,11 +45,16 @@ public class QuestionEditView implements Serializable{
     private QuestionClient questionClient = new QuestionClient();
     private Question question = new Question();
     private List<Question> questions = new ArrayList<Question>();
-
+    
+    private SurveyClient surveyClient = new SurveyClient();
+    private Survey survey = new Survey();
+    private List<Survey> surveys = new ArrayList<Survey>();
+    
     @PostConstruct
     public void init() {
         questions = questionClient.findAll(new GenericType<List<Question>>() {
         });
+        surveys = surveyClient.findAll(new GenericType<List<Survey>>(){});
     }
     public Question getQuestion() {
         return question;
@@ -62,9 +71,29 @@ public class QuestionEditView implements Serializable{
     public void setQuestions(List<Question> questions) {
         this.questions = questions;
     }
+
+    public Survey getSurvey() {
+        return survey;
+    }
+
+    public void setSurvey(Survey survey) {
+        this.survey = survey;
+    }
+
+    public List<Survey> getSurveys() {
+        return surveys;
+    }
+
+    public void setSurveys(List<Survey> surveys) {
+        this.surveys = surveys;
+    }
     
     
     public void onRowEdit(RowEditEvent event) {
+        question = (Question) event.getObject();
+        question.setSurvey(survey);
+        questionClient.edit(question, question.getId().toString());
+
         FacesMessage msg = new FacesMessage("Question Edited", "");
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
@@ -83,8 +112,49 @@ public class QuestionEditView implements Serializable{
             FacesContext.getCurrentInstance().addMessage(null, msg);
         }
     }
-    public void onRowDelete(Question question) {
-        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Deleted", "");
-        FacesContext.getCurrentInstance().addMessage(null, msg);
+//    public void onRowDelete(Question question) {
+//        questionClient.remove(question.getId().toString());
+//        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Deleted", "");
+//        FacesContext.getCurrentInstance().addMessage(null, msg);
+//    }
+    public void eliminarQuestion() {
+        try {
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            String idQuestion = (String) facesContext.getExternalContext().getRequestParameterMap().get("idQuestion");
+
+            if (idQuestion != null && !"".equals(idQuestion)) {
+                questionClient.remove(idQuestion);
+                questions = questionClient.findAll(new GenericType<List<Question>>() {
+                });
+                surveys = surveyClient.findAll(new GenericType<List<Survey>>() {
+                });
+            }
+
+            FacesContext.getCurrentInstance().addMessage("llistQuestions", new FacesMessage(FacesMessage.SEVERITY_INFO, "Deletion succeed", null));
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(
+                    "llistQuestions",
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR when deleting", null));
+        }
+
     }
+    
+    public void recuperarValorCamp(AjaxBehaviorEvent e) {
+        //assign new value to localeCode
+        Integer idSurvey =  (Integer) ((UIOutput) e.getSource()).getValue();
+        survey = surveyClient.find(Survey.class, idSurvey.toString());
+        
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        String idQuestion = (String) facesContext.getExternalContext().getRequestParameterMap().get("idQuestion");
+        
+        for(Question q: questions){
+            if(q.getId().equals(idQuestion)){
+                q.setSurvey(survey);
+                break;
+            }
+        }
+
+    }
+
+    
 }
