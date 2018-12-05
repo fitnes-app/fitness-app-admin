f14352d04871b5ed56871ba156f56b4929595aa8
Jordi Godia <jgm17@alumnes.udl.cat>
2018-12-05 16:47:54 +0100

    Speciality API integration, Create part on process

---
+++ fitnessapp-admin/fitnessapp-admin-web/src/main/java/com/fitnessapp/api/entities/Speciality.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2018 Jordi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.fitnessapp.api.entities;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Jordi
+ */
+public class Speciality implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String specialityName;
+
+    public Speciality() {
+    }
+
+    public Speciality(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSpecialityName() {
+        return specialityName;
+    }
+
+    public void setSpecialityName(String specialityName) {
+        this.specialityName = specialityName;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Speciality)) {
+            return false;
+        }
+        Speciality other = (Speciality) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.fitnessapp.entities.Speciality[ id=" + id + " ]";
+    }
+}
